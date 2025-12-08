a2ad4a51816182faee90132d84119e92c0c8d142
ivycw22 <ivycw22@gmail.com>
2025-12-08 12:35:50 -0500

    db info

--- api/src/db/pool.js
+++ api/src/db/pool.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql01.cs.virginia.edu',
   port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'cavrideshare',
+  user: process.env.DB_USER || 'wyb4kk',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_NAME || 'wyb4kk_c',
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
   namedPlaceholders: true,
